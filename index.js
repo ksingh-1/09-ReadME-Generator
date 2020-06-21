d15636e9166529b841511daeb411f8320f16c98c
SSVF2 <kulpreet.s18@gmail.com>
2020-06-21 01:13:48 -0500

    updating files

--- index.js
+++ index.js
@@ -2,38 +2,51 @@ const fs=require("fs");
 const inquirer=require("inquirer");
 const axios=require("axios");
 const util=require("util");
-const generateMarkdown=require("./markdownGenerator");
+const generateMarkdown=require("./Utils/generateMarkdown");
 const writeFileAsync=util.promisify(fs.writeFile);
 
 const questions=[
     {
+        type: "questions",
         message:"What Is The Name Of The Project?",
         answer: "title"
     },
     {
+        type: "questions",
         message:"What Is This Project About?",
-        answer:"describe"
+        answer:"description"
     },
     {
+        type: "questions",
         message:"What Can This Project Be Used?",
-        answer:"utility"
+        answer:"use"
     },
     {
+        type: "questions",
         message:"Does This Project Have A License?",
         answer: "license"
     },
     {
+        type: "questions",
         message:"Who Worked On This Project?",
-        answer: "people"
+        answer: "contributors"
     },
     {
+        type: "questions",
         message:"What Is Your Contact Preference?",
         answer: "contact"
     },
     {
+        type: "questions",
         message:"What Is Your Username?",
         answer:"username"
     },
+    {
+        type:"questions",
+        message:"What is your email?"
+        answer:"email"
+    },
+
 ];
 
 function askUser() {
