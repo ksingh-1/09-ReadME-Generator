7d37842f80e1f7c8bd80ebaf4d7133f28a10332f
SSVF2 <kulpreet.s18@gmail.com>
2020-07-03 23:55:08 -0500

    updating type to 'input' instead of 'questions' and data instead of 'answers'

--- index.js
+++ index.js
@@ -7,44 +7,44 @@ const writeFileAsync=util.promisify(fs.writeFile);
 
 const questions=[
     {
-        type: "questions",
+        type: "input",
         message:"What Is The Name Of The Project?",
         answer: "title"
     },
     {
-        type: "questions",
+        type: "input",
         message:"What Is This Project About?",
         answer:"description"
     },
     {
-        type: "questions",
+        type: "input",
         message:"What Can This Project Be Used For?",
         answer:"usage"
     },
     {
-        type: "questions",
+        type: "input",
         message:"Does This Project Have A License?",
         answer: "license"
     },
     {
-        type: "questions",
+        type: "input",
         message:"Who Worked On This Project?",
         answer: "contributors"
     },
     {
-        type: "questions",
+        type: "input",
         message:"What Is Your Contact Preference?",
         answer: "contact"
     },
     {
-        type: "questions",
+        type: "input",
         message:"What Is Your Username?",
         answer:"username"
     },
     {
-        type:"questions",
+        type:"input",
         message:"What is your email?"
-        answer:"email"
+        data:"email"
     },
 
 ];
@@ -56,11 +56,11 @@ const questions=[
 // }
 
 // function init () {
-//     return inquirer.prompt(questions)
+//     return inquirer.prompt(input)
 //         .then(function (data){
 //             markdown(data);
 //             const userMarkdown=markdown(data);
-//             api.getUser(data.usernma);
+//             api.getUser(data.username);
 //             writeToFile('${data.title}.md', userMarkdown);
 //             console.log("Success!");
 //         })
@@ -70,23 +70,30 @@ const questions=[
 // }
 // init();
 //
-function askUser() {
+function promptUser() {
     return inquirer.prompt(questions)
 };
 
+function avatarQuery(data) {
+    const queryURL='https://api.github.com/users/${data.Name}';
+    
+    return axios.get(queryURL).then((response) => {
+        const imURL=response.data.avatar_url;
+        return imURL;
+    });
+}
 async function init(){
     console.log("ReadMe Generator 'On'");
-}
 
     try {
-        const answers=await askUser();
-        let github=await axios.get('https://api.github.com/users/${answers.username}')
-        const md=generateMarkdown(answers, github.data);
+        const data=await askUser();
+        let github=await axios.get('https://api.github.com/users/${data.username}')
+        const md=generateMarkdown(data, github.data);
         await writeFileAsync("ReadME.md", md);
         console.log("ReadME file generated");
     } catch (err) {
         console.log(err);
     }
-};
+}
 
 init();
