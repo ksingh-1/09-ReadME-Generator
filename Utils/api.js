bcebf0cf02d9ca390c596817f93eba5edbe0eef0
SSVF2 <kulpreet.s18@gmail.com>
2020-07-04 23:35:46 -0500

    updated apostrophes

--- Utils/api.js
+++ Utils/api.js
@@ -2,7 +2,7 @@ const axiox = require("axios");
 
 const api = {
   getUser(username) {
-    const queryURL='https://api.github.com/users/${username}';
+    const queryURL=`https://api.github.com/users/${username}`;
     axios.get(queryURL)
       .then(function (res){
         console.log(res.data);
